2575d768b771f785060e7ec4cd3ea0cc10aedc99
vikasdeep sharma <vikasdeep.sharma@paytm.com>
2019-12-24 15:54:50 +0530

    duplicate files removed

--- ubuntu-18/scripts/scripts/configure_github.sh
+++
@@ -1,9 +0,0 @@
-cat > /root/.ssh/id_rsa <<- EOM
------BEGIN RSA PRIVATE KEY-----
-private key content
------END RSA PRIVATE KEY-----
-EOM
-chmod 400 /root/.ssh/id_rsa
-
-#To configure ansible_vault
-echo 'W%,cpJ7V' > /root/.ansible_vault
